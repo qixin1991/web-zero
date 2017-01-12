d0cb8cec6a090c59aa9a80e2802db4fe21d3bf90
brainqi <brainqi@outlook.com>
2017-01-13 01:04:55 +0800

    es6 is ok.

--- index.js
+++ index.js
@@ -1,88 +1,93 @@
 #!/usr/bin/env node
 const fs = require('fs'),
-    path = require('path');
-const tpl = require(path.join(__dirname, 'template', 'tpl.js'));
-var pkg = require('./package.json');
-
-const operation = process.argv[2],
+    path = require('path'),
+    tpl = require(path.join(__dirname, 'template', 'tpl.js')),
+    pwd = process.cwd(),
+    operation = process.argv[2],
     option = process.argv[3];
+console.log(pwd);
+var pkg = require(path.join(pwd, 'package.json'));
 
 switch (operation) {
     case 'init':
-        init();
+        var g = init();
+        g.next();
+        g.next();
+        g.next();
         break;
     case 'new':
         if (!option) {
             console.error('please tell me the module name!');
             break;
         }
-        new_module(option);
+        var g = new_module(option);
+        g.next();
+        g.next();
         break;
     default:
         console.log(' Usage: web-zero operation [init | new] option [module_name]');
         break;
 }
-// init();
 
 /**
  * init project.
  */
-async function init() {
-    await init_dir();
-    await init_file();
-    await init_dependencies();
+function* init() {
+    yield init_dir();
+    yield init_file();
+    yield init_dependencies();
 }
 
 /**
  * create route and dao files.
  */
-async function new_module(option) {
-    await new_route(option);
-    await new_dao(option);
+function* new_module(option) {
+    yield new_route(option);
+    yield new_dao(option);
 }
 
 function new_route(option) {
-    fs.writeFile(path.join(__dirname, 'routes', option + '.js'), tpl.base_router.replace(/\$option/g, option), (err) => {
+    fs.writeFile(path.join(pwd, 'routes', option + '.js'), tpl.base_router.replace(/\$option/g, option), (err) => {
         if (err)
             throw err;
-        console.log(` ---> Create routes/${option}.js success...`);
+        console.log(` ---> Create File routes/${option}.js success...`);
     });
 }
 
 function new_dao(option) {
-    fs.writeFile(path.join(__dirname, 'dao', option + '.js'), tpl.base_dao.replace(/\$option/g, option.toUpperCase()), (err) => {
+    fs.writeFile(path.join(pwd, 'dao', option + '.js'), tpl.base_dao.replace(/\$option/g, option.toUpperCase()), (err) => {
         if (err)
             throw err;
-        console.log(` ---> Create dao/${option}.js success...`);
+        console.log(` ---> Create File dao/${option}.js success...`);
     });
 }
 
 /**
- * create route and dao dir.
+ * create route,dao,middleware and conf dir.
  */
 function init_dir() {
-    fs.mkdir(path.join(__dirname, 'routes'), (err) => {
+    fs.mkdir(path.join(pwd, 'routes'), (err) => {
         if (err && err.code !== 'EEXIST')
             throw err;
-        console.log(' ---> Create routes dir success...');
+        console.log(' ---> Create Directory routes success...');
     });
 
-    fs.mkdir(path.join(__dirname, 'dao'), (err) => {
+    fs.mkdir(path.join(pwd, 'dao'), (err) => {
         if (err && err.code !== 'EEXIST')
             throw err;
-        console.log(' ---> Create dao dir success...');
+        console.log(' ---> Create Directory dao success...');
     });
 
-    fs.mkdir(path.join(__dirname, 'middleware'), (err) => {
+    fs.mkdir(path.join(pwd, 'middleware'), (err) => {
         if (err && err.code !== 'EEXIST')
             throw err;
-        console.log(' ---> Create middleware dir success...');
+        console.log(' ---> Create Directory middleware success...');
     });
 
-    fs.mkdir(path.join(__dirname, 'conf'), (err) => {
+    fs.mkdir(path.join(pwd, 'conf'), (err) => {
         if (err && err.code !== 'EEXIST')
             throw err;
-        console.log(' ---> Create middleware dir success...');
+        console.log(' ---> Create Directory conf success...');
     });
 }
 
@@ -90,82 +95,82 @@ function init_file() {
     /**
     * create app.js and write tpl code into it.
     */
-    fs.writeFile(path.join(__dirname, 'app.js'), tpl.app, (err) => {
+    fs.writeFile(path.join(pwd, 'app.js'), tpl.app, (err) => {
         if (err)
             throw err;
-        console.log(' ---> Create app.js success...');
+        console.log(' ---> Create File app.js success...');
     });
 
     /**
      * create routes/base.js
      */
-    fs.writeFile(path.join(__dirname, 'routes', 'base.js'), tpl.base, (err) => {
+    fs.writeFile(path.join(pwd, 'routes', 'base.js'), tpl.base, (err) => {
         if (err)
             throw err;
-        console.log(' ---> Create routes/base.js success...');
+        console.log(' ---> Create File routes/base.js success...');
     });
 
     /**
     * create middleware/log.js
     */
-    fs.writeFile(path.join(__dirname, 'middleware', 'log.js'), tpl.log, (err) => {
+    fs.writeFile(path.join(pwd, 'middleware', 'log.js'), tpl.log, (err) => {
         if (err)
             throw err;
-        console.log(' ---> Create middleware/log.js success...');
+        console.log(' ---> Create File middleware/log.js success...');
     });
 
     /**
      * create config.js | db_development.js | db_production.js | db_staging.js
      */
-    fs.writeFile(path.join(__dirname, 'conf', 'config.js'), tpl.config, (err) => {
+    fs.writeFile(path.join(pwd, 'conf', 'config.js'), tpl.config, (err) => {
         if (err)
             throw err;
-        console.log(' ---> Create conf/config.js success...');
+        console.log(' ---> Create File conf/config.js success...');
     });
 
-    fs.writeFile(path.join(__dirname, 'conf', 'db_development.js'), tpl.db_development, (err) => {
+    fs.writeFile(path.join(pwd, 'conf', 'db_development.js'), tpl.db_development, (err) => {
         if (err)
             throw err;
-        console.log(' ---> Create conf/db_development.js success...');
+        console.log(' ---> Create File conf/db_development.js success...');
     });
 
-    fs.writeFile(path.join(__dirname, 'conf', 'db_staging.js'), tpl.db_staging, (err) => {
+    fs.writeFile(path.join(pwd, 'conf', 'db_staging.js'), tpl.db_staging, (err) => {
         if (err)
             throw err;
-        console.log(' ---> Create conf/db_staging.js success...');
+        console.log(' ---> Create File conf/db_staging.js success...');
     });
 
-    fs.writeFile(path.join(__dirname, 'conf', 'db_production.js'), tpl.db_production, (err) => {
+    fs.writeFile(path.join(pwd, 'conf', 'db_production.js'), tpl.db_production, (err) => {
         if (err)
             throw err;
-        console.log(' ---> Create conf/db_production.js success...');
+        console.log(' ---> Create File conf/db_production.js success...');
     });
 
     /**
      * create mongo.js | redis.js | qiniu.js | mysql.js
      */
-    fs.writeFile(path.join(__dirname, 'dao', 'mongo.js'), tpl.mongo, (err) => {
+    fs.writeFile(path.join(pwd, 'dao', 'mongo.js'), tpl.mongo, (err) => {
         if (err)
             throw err;
-        console.log(' ---> Create dao/mongo.js success...');
+        console.log(' ---> Create File dao/mongo.js success...');
     });
 
-    fs.writeFile(path.join(__dirname, 'dao', 'redis.js'), tpl.redis, (err) => {
+    fs.writeFile(path.join(pwd, 'dao', 'redis.js'), tpl.redis, (err) => {
         if (err)
             throw err;
-        console.log(' ---> Create dao/redis.js success...');
+        console.log(' ---> Create File dao/redis.js success...');
     });
 
-    fs.writeFile(path.join(__dirname, 'dao', 'qiniu.js'), tpl.qiniu, (err) => {
+    fs.writeFile(path.join(pwd, 'dao', 'qiniu.js'), tpl.qiniu, (err) => {
         if (err)
             throw err;
-        console.log(' ---> Create dao/qiniu.js success...');
+        console.log(' ---> Create File dao/qiniu.js success...');
     });
 
-    fs.writeFile(path.join(__dirname, 'dao', 'mysql.js'), tpl.mysql, (err) => {
+    fs.writeFile(path.join(pwd, 'dao', 'mysql.js'), tpl.mysql, (err) => {
         if (err)
             throw err;
-        console.log(' ---> Create dao/mysql.js success...');
+        console.log(' ---> Create File dao/mysql.js success...');
     });
 }
 
@@ -189,7 +194,7 @@ function init_dependencies() {
         "superagent": "^2.1.0"
     }
 
-    fs.writeFile(path.join(__dirname, 'package.json'), JSON.stringify(pkg, null, 4), (err) => {
+    fs.writeFile(path.join(pwd, 'package.json'), JSON.stringify(pkg, null, 4), (err) => {
         if (err)
             throw err;
         console.log(' ---> Add dependencies success...');
