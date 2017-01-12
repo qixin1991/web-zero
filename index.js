013d088d4382fc1aa7964e6f5d2a2a5307ab9d8f
brainqi <brainqi@outlook.com>
2017-01-12 18:17:45 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,153 @@
+const fs = require('fs'),
+    path = require('path');
+const tpl = require(path.join(__dirname, 'template', 'tpl.js'));
+var pkg = require('./package.json');
+
+/**
+ * auto-run
+ */
+(async () => {
+    await init_dir();
+    await init_file();
+    await init_dependencies();
+})();
+
+
+/**
+ * create route and dao dir.
+ */
+function init_dir() {
+    fs.mkdir(path.join(__dirname, 'routes'), (err) => {
+        if (err && err.code !== 'EEXIST')
+            throw err;
+        console.log(' ---> Create routes dir success...');
+    });
+
+    fs.mkdir(path.join(__dirname, 'dao'), (err) => {
+        if (err && err.code !== 'EEXIST')
+            throw err;
+        console.log(' ---> Create dao dir success...');
+    });
+
+    fs.mkdir(path.join(__dirname, 'middleware'), (err) => {
+        if (err && err.code !== 'EEXIST')
+            throw err;
+        console.log(' ---> Create middleware dir success...');
+    });
+
+    fs.mkdir(path.join(__dirname, 'conf'), (err) => {
+        if (err && err.code !== 'EEXIST')
+            throw err;
+        console.log(' ---> Create middleware dir success...');
+    });
+}
+
+function init_file() {
+    /**
+    * create app.js and write tpl code into it.
+    */
+    fs.writeFile(path.join(__dirname, 'app.js'), tpl.app, (err) => {
+        if (err)
+            throw err;
+        console.log(' ---> Create app.js success...');
+    });
+
+    /**
+     * create routes/base.js
+     */
+    fs.writeFile(path.join(__dirname, 'routes', 'base.js'), tpl.base, (err) => {
+        if (err)
+            throw err;
+        console.log(' ---> Create routes/base.js success...');
+    });
+
+    /**
+    * create middleware/log.js
+    */
+    fs.writeFile(path.join(__dirname, 'middleware', 'log.js'), tpl.log, (err) => {
+        if (err)
+            throw err;
+        console.log(' ---> Create middleware/log.js success...');
+    });
+
+    /**
+     * create config.js | db_development.js | db_production.js | db_staging.js
+     */
+    fs.writeFile(path.join(__dirname, 'conf', 'config.js'), tpl.config, (err) => {
+        if (err)
+            throw err;
+        console.log(' ---> Create conf/config.js success...');
+    });
+
+    fs.writeFile(path.join(__dirname, 'conf', 'db_development.js'), tpl.db_development, (err) => {
+        if (err)
+            throw err;
+        console.log(' ---> Create conf/db_development.js success...');
+    });
+
+    fs.writeFile(path.join(__dirname, 'conf', 'db_staging.js'), tpl.db_staging, (err) => {
+        if (err)
+            throw err;
+        console.log(' ---> Create conf/db_staging.js success...');
+    });
+
+    fs.writeFile(path.join(__dirname, 'conf', 'db_production.js'), tpl.db_production, (err) => {
+        if (err)
+            throw err;
+        console.log(' ---> Create conf/db_production.js success...');
+    });
+
+    /**
+     * create mongo.js | redis.js | qiniu.js | mysql.js
+     */
+    fs.writeFile(path.join(__dirname, 'dao', 'mongo.js'), tpl.mongo, (err) => {
+        if (err)
+            throw err;
+        console.log(' ---> Create dao/mongo.js success...');
+    });
+
+    fs.writeFile(path.join(__dirname, 'dao', 'redis.js'), tpl.redis, (err) => {
+        if (err)
+            throw err;
+        console.log(' ---> Create dao/redis.js success...');
+    });
+
+    fs.writeFile(path.join(__dirname, 'dao', 'qiniu.js'), tpl.qiniu, (err) => {
+        if (err)
+            throw err;
+        console.log(' ---> Create dao/qiniu.js success...');
+    });
+
+    fs.writeFile(path.join(__dirname, 'dao', 'mysql.js'), tpl.mysql, (err) => {
+        if (err)
+            throw err;
+        console.log(' ---> Create dao/mysql.js success...');
+    });
+}
+
+/**
+ * add dependencies to package.json
+ */
+function init_dependencies() {
+    pkg.dependencies = {
+        "expect.js": "^0.3.1",
+        "formidable": "^1.0.17",
+        "ioredis": "^2.3.0",
+        "koa": "^2.0.0",
+        "koa-bodyparser": "^3.2.0",
+        "koa-exception": "^2.0.0",
+        "koa-router": "^7.0.1",
+        "moment": "^2.15.1",
+        "mongodb": "^2.2.8",
+        "mysql": "^2.11.1",
+        "qiniu": "^6.1.11",
+        "redlock": "^2.0.1",
+        "superagent": "^2.1.0"
+    }
+
+    fs.writeFile(path.join(__dirname, 'package.json'), JSON.stringify(pkg, null, 4), (err) => {
+        if (err)
+            throw err;
+        console.log(' ---> Add dependencies success...');
+    });
+}
